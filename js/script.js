dc6905b96f54413c740f416d066ba1c50550dce7
Alvaro Torres <torrestal@gmail.com>
2018-12-03 10:08:12 +0100

    creating commits inside an array

--- js/script.js
+++ js/script.js
@@ -54,12 +54,17 @@ function draw(last) {
       last.draw();
     }
 }
+var commits = new Array();
+commits.push(new Commit('Naces un día', null));
+commits.push(new Commit('Creces y creces', commits[commits.length - 1]));
+commits.push(new Commit('Vas al colegio', commits[commits.length - 1]));
+commits.push(new Commit('Aprendes memeces', commits[commits.length - 1]));
 
-var first = new Commit('Naces un día', null);
-var c2 = new Commit('Creces y creces', first);
-var c3 = new Commit('Vas al colegio', c2);
-var c4 = new Commit('Aprendes memeces', c3);
+//var first = new Commit('Naces un día', null);
+//var c2 = new Commit('Creces y creces', first);
+//var c3 = new Commit('Vas al colegio', c2);
+//var c4 = new Commit('Aprendes memeces', c3);
 
-draw(first);
+draw(commits[0]);
 
 document.getElementById('svg').innerHTML = svg_html;
